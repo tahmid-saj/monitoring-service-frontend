da48ed83037ff04b4670f8c502fcb216efebf44a
tahmid-saj <tahmid.saj@gmail.com>
2024-09-23 21:26:31 -0400

    monitoring shared components and button / header

---
+++ src/components/monitoring/header/header.component.tsx
@@ -0,0 +1,12 @@
+import "./header.styles"
+import { HeaderContainer } from "./header.styles"
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <h2>Monitoring Service</h2>
+    </HeaderContainer>
+  )
+}
+
+export default Header
--- src/components/monitoring/monitoring.component.tsx
+++ src/components/monitoring/monitoring.component.tsx
@@ -1,8 +1,14 @@
+import { Fragment } from "react/jsx-runtime"
 import "./monitoring.styles"
+import Header from "./header/header.component"
+import AddWebsite from "./add-website/add-website.component"
 
 const Monitoring = () => {
   return (
-    
+    <Fragment>
+      <Header/>
+      <AddWebsite/>
+    </Fragment>
   )
 }
 
